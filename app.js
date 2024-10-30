724d31931a9548c8d5ee3dfc02d72634282f8e3c
Silvio Sales do Nascimento Junior <silviosnjr@gmail.com>
2024-10-30 14:37:13 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-function criaCartao(categoria, pergunta, resposta, imgResposta, fake){
+function criaCartao(categoria, pergunta, resposta, imgPergunta, imgResposta, secreto){
     let container = document.getElementById('container')
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
 
     cartao.innerHTML = `
-    <div class="cartao__conteudo">
-    <h3>${categoria}</h3>
+    <div class="cartao__conteudo ${secreto}">
+    <!--<h3>${categoria}</h3>-->
     <div class="cartao__conteudo__pergunta">
         <p>${pergunta}</p>
-        <img src="assets/img/wizard.png">
+        <img src="assets/img/${imgPergunta}">
     </div>
-    <div class="cartao__conteudo__resposta ${fake}">
+    <div class="cartao__conteudo__resposta">
         <p>${resposta}</p>
         <img src="assets/img/${imgResposta}">
     </div>
